abee31be47fb00242c56e1ee7c235413652b6214
juankristo <90241034+juankristo@users.noreply.github.com>
2021-09-30 12:49:26 +0800

    Add files via upload

---
+++ hadiahprak2_nomor1.java
@@ -0,0 +1,19 @@
+//Genap 
+//NIM : 51019014
+public class hadiahprak2_nomor1 {
+	
+	public static void main(String[] args) {
+	
+	float panjang,lebar,luas;
+	
+	panjang=2;
+	lebar=5;
+	
+	System.out.println("Panjang Persegi Panjang : " +panjang);
+	System.out.println("Lebar Persegi Panjang : " +lebar);
+	System.out.println("\n");
+	luas=panjang*lebar;
+	System.out.println("Hasil Luas Persegi Panjang : " + luas);
+	
+
+}}
